776ea257719e7172a814ca7b605bea3a23c9cc2e
Juo Tung Chen <95962993+JuoTungChen@users.noreply.github.com>
2024-02-28 17:14:19 -0500

    Add files via upload

--- joy_teleop/scripts/vive_teleop.py
+++ joy_teleop/scripts/vive_teleop.py
@@ -2,9 +2,13 @@
 # -*- coding: utf-8 -*-
 import sys
 import rospy
+import math
 from geometry_msgs.msg import Twist, PoseStamped, TwistStamped
 from sensor_msgs.msg import Joy
 import actionlib
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+
+from geometry_msgs.msg import TransformStamped, Pose
 from teleop_tools_msgs.msg import IncrementAction as TTIA
 from teleop_tools_msgs.msg import IncrementGoal as TTIG
 from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal
@@ -12,9 +16,23 @@ from trajectory_msgs.msg import JointTrajectoryPoint
 from controller_manager_msgs.srv import SwitchController
 from moveit_msgs.msg import MoveItErrorCodes
 # from moveit_python import MoveGroupInterface, PlanningSceneInterface
+
+
 import moveit_commander
 class JoystickTeleop():
     def __init__(self):
+
+        ##-------- Variables---------## 
+        self.right_controller_x, self.right_controller_y, self.right_controller_z = 0, 0, 0
+        self.right_controller_rx, self.right_controller_ry, self.right_controller_rz, self.right_controller_rw = 0, 0, 0, 0
+
+        self.left_controller_x, self.left_controller_y, self.left_controller_z = 0, 0, 0
+        self.left_controller_rx, self.left_controller_ry, self.left_controller_rz, self.left_controller_rw = 0, 0, 0, 0
+
+        self.alpha = 0.7
+        self.initialized = False
+        self.filtered_orientation = [0.0, 0.0]  # Initial filtered orientation
+        self.prev_head_pos = [0.0, 0.0]
         moveit_commander.roscpp_initialize(sys.argv)
         self.robot = moveit_commander.RobotCommander()
         self.scene = moveit_commander.PlanningSceneInterface()
@@ -22,7 +40,9 @@ class JoystickTeleop():
         self.left_move_group = moveit_commander.MoveGroupCommander(self.left_group_name)
         # self.left_move_group.setPlannerId("SBLkConfigDefault")
         self._pub_cmd = rospy.Publisher('key_vel', Twist, queue_size=10)
-        self._joy_subscriber = rospy.Subscriber('joy', Joy, self._joy_callback)
+        # self._joy_subscriber = rospy.Subscriber('joy', Joy, self._joy_callback)
+
+
         self.torso_client = actionlib.SimpleActionClient('/torso_controller/increment', TTIA)
         self.head_client = actionlib.SimpleActionClient('/head_controller/follow_joint_trajectory', FollowJointTrajectoryAction)
         self.left_gripper_client = actionlib.SimpleActionClient('/parallel_gripper_left_controller/follow_joint_trajectory', FollowJointTrajectoryAction)
@@ -45,81 +65,105 @@ class JoystickTeleop():
         self.start_controller=['arm_left_JGP']
         self.stop_controller=['arm_left_controller', 'torso_controller']
         self.arm_left_home_joint_angles = [0.1404634725889998, -1.0999085872589465, 1.4541173782490207, 2.720822327116011, 1.6583055087691845, -1.582990280917917, 1.3793348919241852, 0.0003437668173003061]
+
+
+        self.head_joint_limits = [[-1.24, 1.24], [-0.98, 0.72]]
+
         rospy.sleep(0.5)
 
-    def run(self):
-        rate = rospy.Rate(self._hz)
-        self._running = True
-        while self._running and not rospy.is_shutdown():
-            rate.sleep()
+        rospy.Subscriber('/Right_Hand', TransformStamped, self.__right_input_pose_callback)
+        rospy.Subscriber('/Right_Buttons', Joy, self.__right_input_buttons_callback)
+        rospy.Subscriber('/Left_Hand', TransformStamped, self.__left_input_pose_callback)
+        rospy.Subscriber('/Left_Buttons', Joy, self.__left_input_buttons_callback)
+        rospy.Subscriber('/Head_Motion', PoseStamped, self.__head_motion_callback)
 
-    def _joy_callback(self, joy_msg):
-        # Check button 2 for base mode and button 3 for head mode
-        if joy_msg.buttons[2] and not self._mode == 'base':
-            rospy.logwarn("changed to base mode")
-            self._change_controller(self.stop_controller, self.start_controller)
 
-            self._mode = 'base'
-        elif joy_msg.buttons[3] and not self._mode == 'head':
-            rospy.logwarn("changed to head mode")
-            self._mode = 'head'
-        elif joy_msg.buttons[1] and not self._mode == 'arm':
-            rospy.logwarn("changed to arm mode")
-            self._mode = 'arm'
+    ##-------- Callback Functions---------## 
 
-            
+    def __right_input_pose_callback(self, msg):
 
-        if (joy_msg.buttons[0] and self._mode == 'arm'):
-            self.translation_mode = not self.translation_mode
-            print("Translation mode:", self.translation_mode)
-            rospy.sleep(0.1)
+        self.right_controller_x = msg.transform.translation.x
+        self.right_controller_y = msg.transform.translation.y
+        self.right_controller_z = msg.transform.translation.z
+        self.right_controller_rx, self.right_controller_ry, self.right_controller_rz, self.right_controller_rw = msg.transform.rotation.x, msg.transform.rotation.y, msg.transform.rotation.z, msg.transform.rotation.w
 
-        # Check the current mode and execute the corresponding control
-        if self._mode == 'base':
-            
-            self._base_control(joy_msg)
-        elif self._mode == 'head':
-            self._head_control(joy_msg)
-        elif self._mode == 'arm':
-            if(joy_msg.buttons[4] and not self.arm_side == "left"):
-                self.arm_side = "left"
-                self._change_controller(self.start_controller, self.stop_controller)
-                rospy.logwarn("switched to left arm")
-            elif(joy_msg.buttons[5] and not self.arm_side == "right"):
-                self.arm_side = "right"
-                rospy.logwarn("switched to right arm")
-            if joy_msg.axes[6] == 1.0:
-                rospy.logwarn("left arm move to home...")
-                self._move_to_home_position()  
-            elif joy_msg.axes[6] == -1.0:
-                pass
+    def __right_input_buttons_callback(self, msg):
+        """
+            determine if it is squeezing or button press 
 
-            self._left_arm_teleop_control(joy_msg)
-            # self._arm_cartesian_control(joy_msg)
-            self._gripper_control(joy_msg)
-            
+        """
+        self._base_control(msg)
+        if msg.buttons[0] == 1:
+            self._torso_increment(0.05)  # Increment for torso up
         self._publish()
 
+
+    def __left_input_pose_callback(self, msg):
+
+        self.left_controller_x = msg.transform.translation.x
+        self.left_controller_y = msg.transform.translation.y
+        self.left_controller_z = msg.transform.translation.z
+        self.left_controller_rx, self.left_controller_ry, self.left_controller_rz, self.left_controller_rw = msg.transform.rotation.x, msg.transform.rotation.y, msg.transform.rotation.z, msg.transform.rotation.w
+
+    def __left_input_buttons_callback(self, msg):
+        """
+            control mobile base using the trackpad on the left controller
+
+        """
+        self._base_rotate(msg)
+
+        if msg.buttons[0] == 1:
+            self._torso_increment(-0.05)  # Increment for torso up
+        self._publish()
+
+
+    def __head_motion_callback(self, msg):
+        """
+            control mobile base using the trackpad on the left controller
+
+        """
+        if not self.initialized:
+            self.initialized = True
+            self.filtered_orientation = [msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w]
+
+
+        # Apply the low-pass filter
+        filtered_orientation_x = self.alpha * msg.pose.orientation.x + (1 - self.alpha) * self.filtered_orientation[0]
+        filtered_orientation_y = self.alpha * msg.pose.orientation.y + (1 - self.alpha) * self.filtered_orientation[1]
+        filtered_orientation_z = self.alpha * msg.pose.orientation.z + (1 - self.alpha) * self.filtered_orientation[2]
+        filtered_orientation_w = self.alpha * msg.pose.orientation.w + (1 - self.alpha) * self.filtered_orientation[3]
+
+        roll, pitch, yaw = euler_from_quaternion([filtered_orientation_x, filtered_orientation_y, filtered_orientation_z, filtered_orientation_w])
+        # roll, pitch, yaw = euler_from_quaternion([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
+
+        self.filtered_orientation = [filtered_orientation_x, filtered_orientation_y, filtered_orientation_z, filtered_orientation_w]
+
+
+        head_orientation = [round(self.bound(yaw, self.head_joint_limits[1][0], self.head_joint_limits[1][1]), 2),
+                            round(self.bound(-pitch, self.head_joint_limits[0][0], self.head_joint_limits[0][1]), 2)]
+
+        # rospy.logwarn("head orientation = %s, %s",round(math.degrees(head_orientation[0])), round(math.degrees(head_orientation[1])))
+
+        self._send_head_goal(head_orientation)
+        
+
+
+    ##-------- Helper Functions---------## 
+    def bound(self, low, high, value):
+         return max(low, min(high, value))
+
     def _base_control(self, joy_msg):
-        # if joy_msg.button[0] and joy_msg.button[2]:
-
-        # else:   
         linear = joy_msg.axes[1]
-        linear_y = joy_msg.axes[0]
-        angular = joy_msg.axes[3]
-        self._linear = linear * self._forward_rate if linear > 0 else linear * self._backward_rate
-        self._linear_y = linear_y * self._forward_rate if linear > 0 else linear_y * self._backward_rate
-        self._angular = angular * self._rotation_rate
+        linear_y = -joy_msg.axes[0]
+        self._linear = linear * self._forward_rate if linear > 0 else linear * 0.5
+        self._linear_y = linear_y * self._forward_rate if linear > 0 else linear_y * 0.5
 
-        # Check button 4 for torso up and button 5 for torso down
-        if joy_msg.axes[7] == 1.0:
-            self._torso_increment(0.05)  # Increment for torso up
-        elif joy_msg.axes[7] == -1.0:
-            self._torso_increment(-0.05)  # Increment for torso down
+    def _base_rotate(self, msg):
+        angular = -msg.axes[0]
+        self._angular = angular * 1.0
 
 
-
-    def _head_control(self, joy_msg):
+    def _head_control(self, msg):
         # Adjust head position based on joystick axes
         if len(joy_msg.axes) >= 2:
             head_position = [joy_msg.axes[0], joy_msg.axes[1]]
@@ -272,18 +316,25 @@ class JoystickTeleop():
 
 
     def _send_head_goal(self, head_position):
-        # Create a trajectory point with the adjusted head position
-        point = JointTrajectoryPoint()
-        point.positions = head_position
-        point.velocities = [0.1, 0.1]  # Set desired joint velocities
-        point.time_from_start = rospy.Duration(0.1)  # A small duration for smooth motion
 
-        # Create and send the head goal
-        head_goal = FollowJointTrajectoryGoal()
-        head_goal.trajectory.joint_names = ["head_1_joint", "head_2_joint"]
-        head_goal.trajectory.points = [point]
-        head_goal.trajectory.header.stamp = rospy.Time.now()
-        self.head_client.send_goal(head_goal)
+        if not ((abs(self.prev_head_pos[0] - head_position[0]) <= 0.01) and (abs(self.prev_head_pos[1] - head_position[1]) <= 0.01)):
+
+            # Create a trajectory point with the adjusted head position
+            point = JointTrajectoryPoint()
+            point.positions = head_position
+            point.velocities = [abs(self.prev_head_pos[0] - head_position[0]), abs(self.prev_head_pos[1] - head_position[1])]  # Set desired joint velocities
+            point.time_from_start = rospy.Duration(0.1)  # A small duration for smooth motion
+
+            # Create and send the head goal
+            head_goal = FollowJointTrajectoryGoal()
+            head_goal.trajectory.joint_names = ["head_1_joint", "head_2_joint"]
+            head_goal.trajectory.points = [point]
+            head_goal.trajectory.header.stamp = rospy.Time.now()
+            self.head_client.send_goal(head_goal)
+        else:
+            pass
+
+        self.prev_head_pos = head_position
 
     def _torso_increment(self, increment_value):
         rospy.loginfo("Torso increment: {}".format(increment_value))
@@ -299,11 +350,15 @@ class JoystickTeleop():
         return twist
 
     def _publish(self):
-        if self._mode == 'base':
-            twist = self._get_twist(self._linear, self._linear_y, self._angular)
-        else:
-            twist = self._get_twist(0.0, 0.0, 0.0)
+        twist = self._get_twist(self._linear, self._linear_y, self._angular)
         self._pub_cmd.publish(twist)
+
+    def run(self):
+        rate = rospy.Rate(self._hz)
+        self._running = True
+        while self._running and not rospy.is_shutdown():
+            rate.sleep()
+
 
 if __name__ == '__main__':
     try:
